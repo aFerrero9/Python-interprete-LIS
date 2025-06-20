be9953642438d5af8b115ee56b7561eeb4a869a8
andres <andres.ferrero@mi.unc.edu.ar>
2025-06-20 12:46:22 -0300

    Modifico definición de omega

--- datatypes.py
+++ datatypes.py
@@ -1,5 +1,13 @@
 # Intérprete de LIS+fallas+I/O en Python
-
+"""
+Interpretación Omega:
+Es una abstracción del omega real. 
+- status indica si tenemos algo de iota_term(normal),
+o algo de iota_abort(abort).
+- no tenemos iota_out, la abstracción se hace definiendo
+la lista que representan los valores impresos(out).
+- iota_in no existe, simplemente le pedimos el input al usuario en el momento.
+"""
 # Estado: mapeo de variables a enteros
 class State(dict):
     def __init__(self, state: dict = {}):
@@ -7,10 +15,9 @@ class State(dict):
 
 # Omega
 class Omega:
-    def __init__(self, status, state: State, out, inp):
+    def __init__(self, status, state: State, out: list[int]):
         self.status = status  # "normal" o "abort"
         self.state = state    # estado actual
         self.out = out        # lista acumulada
-        self.inp = inp        # resto de la lista de input
     # EXTENDER CON F DE TRANSFERENCIA DE CONTROL
 
